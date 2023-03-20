c7c130a3a94cb96b095a29afbb2f3dc8859356ca
Ajay.Kumar <ajaykumarsd>
2023-03-20 11:42:48 +0530

    package setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import './index.css'
-import App from './App'
-import { render } from 'react-dom'
-import reportWebVitals from './reportWebVitals'
+import App from './App.js'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = document.getElementById('root')
-render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	root
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
 
 // If you want to start measuring performance in your app, pass a function
